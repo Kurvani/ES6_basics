9046a0565317e623d3bfa68d1d7ea2924e9de5cf
Kurvani <kkeyvan1@asu.edu>
2018-11-06 12:07:15 -0700

    Classes and inheritance completed

--- main.js
+++ main.js
@@ -8,12 +8,36 @@ class User {
     this.password = password;
   }
 
+  static countUsers() {
+    console.log("There are 50 users");
+  }
+
   //methods are just functions that belond to a class
   register() {
     console.log(this.username + " is now registered");
   }
 }
 
-let bob = new User("bob", "bob@email.com", "12345encrypted");
+//let bob = new User("bob", "bob@email.com", "12345encrypted");
 
-bob.register();
+//bob.register();
+
+//User.countUsers();
+
+class Member extends User {
+  constructor(username, email, password, memberPackage) {
+    super(username, email, password); //will take the properties from the User class w/out us needs to copy/paste it all
+    this.package = memberPackage;
+  }
+
+  getPackage() {
+    console.log(
+      this.username + " is subscribed to the " + this.package + " package"
+    );
+  }
+}
+
+let mike = new Member("mike", "mike@email.com", "password123", "Standard");
+
+//mike.getPackage();
+mike.register();
