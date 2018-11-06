8236582ec53f1981b5d68524ed430b20f890e582
Kurvani <kkeyvan1@asu.edu>
2018-11-06 12:28:13 -0700

    completed strings/numbers basics

---
+++ js/strings.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let theString = "Hello, my name is Kurosh and I am learning Javascript";
+
+// startsWith()
+// endsWith()
+// includes()
+
+// console.log(theString.startsWith("Hello"));
+// console.log(theString.endsWith("Javascript"));
+// console.log(theString.includes("am"));
+
+// // Hex
+// console.log(0xff);
+// // Binary
+// console.log(0b101011);
+// // Octal
+// console.log(0o543);
+
+// console.log(Number.isFinite(NaN)); // NaN = Not a Number
+
+// console.log(Number.isNaN("string"));
+
+//console.log(Number.isInteger(-2));
